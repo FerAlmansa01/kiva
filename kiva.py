811545095e8cd1edad6af6933049111e73f41ea0
FerAlmansa01 <62451495+FerAlmansa01@users.noreply.github.com>
2021-08-12 16:53:48 +0100

    Remove unnecessary commented out code in main file

--- kiva.py
+++ kiva.py
@@ -61,37 +61,3 @@ if loan_id_input != "":
 
 st.sidebar.markdown('# 🧭 Navigation')
 st.sidebar.radio('Go to', ['🏠 Home','📝 Project Description', '🔬 Model Explainability', '🧑 About'])
-
-
-import cv2
-import numpy as np
-import streamlit as st
-
-#uploaded_file = st.file_uploader("C:\\Users\\Windows\\Documents\\PYTHON\\SHAP_summary_plot.png", type="png")
-d = """
-if uploaded_file is not None:
-    # Convert the file to an opencv image.
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
-
-    # Now do something with the image! For example, let's display it:
-    st.image(opencv_image, channels="BGR")
-    """
-#summary_plot = Image.open('C:\\Users\\Windows\\Documents\\PYTHON\\SHAP_summary_plot.png')
-##st.image(summary_plot, caption='SHAP summary plot')
-
-c = """
-with st.form(key='Form2'):
-    c4, c5, c6 = st.beta_columns((1,1,1))
-    c4.slider(label = 'Num Lenders 1', min_value=0, max_value=10)
-    c4.text_input('Loan Name')
-    c5.slider(label = 'Num Lenders 3 ', min_value=0, max_value=10)
-    c5.slider(label = 'Num Lenders 4', min_value=0, max_value=10)
-    c5.slider(label = 'Num Lenders 5', min_value=0, max_value=10)
-    c6.slider(label = 'Num Lenders 6', min_value=0, max_value=10)
-    c6.slider(label = 'Num Lenders 7', min_value=0, max_value=10)
-    c6.slider(label = 'Num Lenders 8', min_value=0, max_value=10)
-    c4.slider(label = 'Num Lenders 9', min_value=0, max_value=10)
-    submitted = st.form_submit_button('Submit')
-"""    
-    
